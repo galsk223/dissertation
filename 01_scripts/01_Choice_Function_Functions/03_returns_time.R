1a0b0a7c31dfe418611a44f273005c7e5f11bcb0
Gal <sambk223@gmail.com>
2025-10-08 14:58:06 -0700

    SI check (smaller edge list)

--- 01_scripts/01_Choice_Function_Functions/03_returns_time.R
+++ 01_scripts/01_Choice_Function_Functions/03_returns_time.R
@@ -1,67 +1,70 @@
 graph_time_trans <- function(cache_dfchoice, burnin, yi, nvessels, largestcc){
 
-  ey <- transition5(cache_dfchoice, yi) %>% 
+  ey <- transition5(cache_dfchoice, yi) %>%
     filter(weight > 0)
-  
+
   vy <- bind_rows(cache_dfchoice[[yi-4]],
                   cache_dfchoice[[yi-3]],
                   cache_dfchoice[[yi-2]],
                   cache_dfchoice[[yi-1]],
-                  cache_dfchoice[[yi]], .id = "source") %>% 
-    group_by(FISHERY_ID) %>% 
+                  cache_dfchoice[[yi]], .id = "source") %>%
+    group_by(FISHERY_ID) %>%
     summarise(UniqueVessels = n_distinct(Vessel_ID),
-              .groups = "drop") %>% 
+              .groups = "drop") %>%
     filter(FISHERY_ID != "Not Fishing") %>%
-    select(FISHERY_ID, UniqueVessels) 
-  
+    select(FISHERY_ID, UniqueVessels)
+
   # print(setdiff(unique(c(ey$F1, ey$F2)), vy$FISHERY_ID))
   gy <- graph_from_data_frame(ey,
                               directed = F,
                               vertices = vy)
-  
+
   if(largestcc == T){
-    gy <- induced_subgraph(gy, 
-                           vids = which(igraph::components(gy)$membership == 
+    gy <- induced_subgraph(gy,
+                           vids = which(igraph::components(gy)$membership ==
                                           which.max(igraph::components(gy)$csize)))
-    vy <- vy %>% 
+    vy <- vy %>%
       filter(FISHERY_ID %in% names(V(gy)))
     }
-  
+
   A <- as_adjacency_matrix(gy, sparse=FALSE, attr="weight")
   lA <- A[lower.tri(A)]
   Ainv <- A
   Ainv[A!=0] <- 1/A[A!=0]
-  
+
   # graph level
-  OnnelaClust <- ClustF(A, "undirected") 
+  OnnelaClust <- ClustF(A, "undirected")
   cw <- cluster_walktrap(gy, modularity = T)
   m <- modularity(cw)
   nc <- networkcentralization(gy)
   urf <- mean(E(gy)$weight) + var(E(gy)$weight)/mean(E(gy)$weight)
-  
+
   # SI <- sparsityindex(ey, nlinks = nvessels, nnodes = nrow(vy), F)
-  df_sub <- bind_rows(cache_dfchoice[10:14]) %>% 
+  df_sub <- bind_rows(cache_dfchoice[10:14]) %>%
     filter(FISHERY_ID %in% vy$FISHERY_ID)
   nv <- n_distinct(df_sub$Vessel_ID)
-  
-  SI <- sparsityindex(ey, nlinks = nv, nnodes = n_distinct(df_sub$FISHERY_ID), "tt")
-  
+  esi <- ey %>%
+    filter(F1 %in% vy$FISHERY_ID,
+           F2 %in% vy$FISHERY_ID)
+
+  SI <- sparsityindex(esi, nlinks = nv, nnodes = n_distinct(df_sub$FISHERY_ID), "tt")
+
   p <- vy$UniqueVessels/sum(vy$UniqueVessels)
   H <- -sum(p * log(p))
   DF <- fragmentation(Ainv)
-  
+
   # node level
   if (nrow(Ainv) > 2){
     fc <- fragment(Ainv, binary = F)[,1]
   } else {
     fc <- 0
   }
-  
+
   closeness <- igraph::closeness(gy)
   strength <- igraph::strength(gy)
-  
+
   embstats <- fleet_summary_emb_safe(gy, .01)
-  
+
   dfgraph_out <- tibble(Year = yi,
                         Fishery = names(membership(cw)),
                         Size = filter(vy, FISHERY_ID %in% names(membership(cw)))$UniqueVessels,
@@ -84,72 +87,75 @@ graph_time_trans <- function(cache_dfchoice, burnin, yi, nvessels, largestcc){
                         HullAreaTrimmed = embstats$hull_area_trimmed,
                         MedianToCentroid = embstats$median_dist_to_centroid,
                         MeanToCentroid = embstats$mean_dist_to_centroid)
-  
+
 }
 
 graph_time_corr <- function(cache_dfchoice, yi, nvessels, largestcc){
-  
+
   ey <- coredges(cache_dfchoice, yi) %>%
     filter(weight > 0)
-  
+
   vy <- bind_rows(cache_dfchoice[[yi-4]],
                   cache_dfchoice[[yi-3]],
                   cache_dfchoice[[yi-2]],
                   cache_dfchoice[[yi-1]],
-                  cache_dfchoice[[yi]], .id = "source") %>% 
-    group_by(FISHERY_ID)  %>% 
+                  cache_dfchoice[[yi]], .id = "source") %>%
+    group_by(FISHERY_ID)  %>%
     summarise(NWeeks = n(),
-              .groups = "drop") %>% 
+              .groups = "drop") %>%
     filter(FISHERY_ID != "Not Fishing") %>%
-    select(FISHERY_ID, NWeeks) 
+    select(FISHERY_ID, NWeeks)
 
   gy <- graph_from_data_frame(ey,
                               directed = F,
                               vertices = vy)
-  
+
   if(largestcc == T){
-    gy <- induced_subgraph(gy, 
-                           vids = which(igraph::components(gy)$membership == 
+    gy <- induced_subgraph(gy,
+                           vids = which(igraph::components(gy)$membership ==
                                           which.max(igraph::components(gy)$csize)))
-    vy <- vy %>% 
+    vy <- vy %>%
       filter(FISHERY_ID %in% names(V(gy)))
   }
-  
+
   A <- as_adjacency_matrix(gy, sparse=FALSE, attr="weight")
   lA <- A[lower.tri(A)]
   Ainv <- A
   Ainv[A!=0] <- 1/A[A!=0]
-  
+
   # graph level
-  OnnelaClust <- ClustF(A, "undirected") 
+  OnnelaClust <- ClustF(A, "undirected")
   cw <- cluster_walktrap(gy, modularity = T)
   m <- modularity(cw)
   nc <- networkcentralization(gy)
   urf <- mean(E(gy)$weight) + var(E(gy)$weight)/mean(E(gy)$weight)
-  
+
   # SI <- sparsityindex(ey, nlinks = nvessels, nnodes = nrow(vy), F)
-  df_sub <- bind_rows(cache_dfchoice[10:14]) %>% 
+  df_sub <- bind_rows(cache_dfchoice[10:14]) %>%
     filter(FISHERY_ID %in% vy$FISHERY_ID)
   nv <- n_distinct(df_sub$Vessel_ID)
-  
-  SI <- sparsityindex(ey, nlinks = nv, nnodes = n_distinct(df_sub$FISHERY_ID), "tc")
-  
+  esi <- ey %>%
+    filter(Fishery1 %in% vy$FISHERY_ID,
+           Fishery2 %in% vy$FISHERY_ID)
+
+  SI <- sparsityindex(esi, nlinks = nv, nnodes = n_distinct(df_sub$FISHERY_ID), "tc")
+
   p <- vy$NWeeks/sum(vy$NWeeks)
   H <- -sum(p * log(p))
   DF <- fragmentation(Ainv)
-  
+
   # node level
   if (nrow(Ainv) > 2){
     fc <- fragment(Ainv, binary = F)[,1]
   } else {
     fc <- 0
   }
-  
+
   closeness <- igraph::closeness(gy)
   strength <- igraph::strength(gy)
-  
+
   embstats <- fleet_summary_emb_safe(gy, .01)
-  
+
   dfgraph_out <- tibble(Year = yi,
                         Fishery = names(membership(cw)),
                         Size = filter(vy, FISHERY_ID %in% names(membership(cw)))$NWeeks,
@@ -172,5 +178,5 @@ graph_time_corr <- function(cache_dfchoice, yi, nvessels, largestcc){
                         HullAreaTrimmed = embstats$hull_area_trimmed,
                         MedianToCentroid = embstats$median_dist_to_centroid,
                         MeanToCentroid = embstats$mean_dist_to_centroid)
-  
+
 }
--- 01_scripts/01_Choice_Function_Functions/03_returns_vessel.R
+++ 01_scripts/01_Choice_Function_Functions/03_returns_vessel.R
@@ -3,11 +3,11 @@
 # ext ---------------------------------------------------------------------
 
 graph_ext_fcn_ves <- function(df_choice, fisherylist_use, vessel_pairs, yi, drop){
-  
+
   nfisheries <- length(fisherylist_use)
-  dropfishers <- df_choice %>% 
+  dropfishers <- df_choice %>%
     filter(FISHERY_ID %in% drop)
-  
+
   edges_ext <- df_choice %>%
     filter(FISHERY_ID != "Not Fishing") %>%
     distinct(Vessel_ID, FISHERY_ID) %>%
@@ -25,15 +25,15 @@ graph_ext_fcn_ves <- function(df_choice, fisherylist_use, vessel_pairs, yi, drop
     mutate(SharedFisheries = replace_na(SharedFisheries, 0),
            V1 = pmin(as.character(Vessel1), as.character(Vessel2)),
            V2 = pmax(as.character(Vessel1), as.character(Vessel2))) %>%
-    as.data.table() %>% 
+    as.data.table() %>%
     distinct(V1, V2, SharedFisheries) %>%
-    rename(Vessel1 = V1, Vessel2 = V2) %>% 
-    arrange(Vessel1, Vessel2) 
-  
+    rename(Vessel1 = V1, Vessel2 = V2) %>%
+    arrange(Vessel1, Vessel2)
+
   ey <- edges_ext %>%
     filter(SharedFisheries > 0) %>%
     select(Vessel1, Vessel2, weight = SharedFisheries)
-  
+
   vy <- df_choice %>%
     filter(FISHERY_ID != "Not Fishing",
            Vessel_ID %in% c(ey$Vessel1, ey$Vessel2)) %>%
@@ -41,18 +41,18 @@ graph_ext_fcn_ves <- function(df_choice, fisherylist_use, vessel_pairs, yi, drop
     summarise(NFisheries = n_distinct(FISHERY_ID),
               .groups = "drop")  %>%
     select(Vessel_ID, NFisheries)
-  
+
   gy <- graph_from_data_frame(ey,
                               directed = F,
                               vertices = vy)
-  
-  componentuse <- tibble(comps = igraph::components(gy)$membership) %>% 
-    group_by(comps) %>% tally() %>% 
+
+  componentuse <- tibble(comps = igraph::components(gy)$membership) %>%
+    group_by(comps) %>% tally() %>%
     filter(n > 20)
-  
-  i <- unique(componentuse$comps)[2]
+
+  i <- unique(componentuse$comps)[1]
   dfgraph_out_ext <- map_dfr(unique(componentuse$comps), function(i){
-    
+
     vy <- df_choice %>%
       filter(FISHERY_ID != "Not Fishing",
              Vessel_ID %in% c(ey$Vessel1, ey$Vessel2)) %>%
@@ -60,21 +60,21 @@ graph_ext_fcn_ves <- function(df_choice, fisherylist_use, vessel_pairs, yi, drop
       summarise(NFisheries = n_distinct(FISHERY_ID),
                 .groups = "drop")  %>%
       select(Vessel_ID, NFisheries)
-    
+
     gy <- graph_from_data_frame(ey,
                                 directed = F,
                                 vertices = vy)
-    
-    gy <- induced_subgraph(gy, 
+
+    gy <- induced_subgraph(gy,
                            vids = which(igraph::components(gy)$membership == i))
-    vy <- vy %>% 
+    vy <- vy %>%
       filter(Vessel_ID %in% names(V(gy)))
-    
+
     A <- as_adjacency_matrix(gy, sparse=FALSE, attr="weight")
     lA <- A[lower.tri(A)]
     Ainv <- A
     Ainv[A!=0] <- 1/A[A!=0]
-    
+
     # graph level
     OnnelaClust <- ClustF(A, "undirected")
     cw <- cluster_walktrap(gy, modularity = T)
@@ -83,21 +83,24 @@ graph_ext_fcn_ves <- function(df_choice, fisherylist_use, vessel_pairs, yi, drop
     urf <- mean(E(gy)$weight) + var(E(gy)$weight)/mean(E(gy)$weight)
 
     # SI <- sparsityindex(ey, nlinks = nfisheries, nnodes = nrow(vy), F)
-    df_sub <- bind_rows(cache_dfchoice[10:14]) %>% 
+    df_sub <- bind_rows(cache_dfchoice[10:14]) %>%
       filter(Vessel_ID %in% vy$Vessel_ID)
     nf <- n_distinct(df_sub$FISHERY_ID)
-    
-    SI <- sparsityindex(ey, nlinks = nf, nnodes = nrow(vy), "ev")
-    
+    esi <- ey %>%
+      filter(Vessel1 %in% vy$Vessel_ID,
+             Vessel2 %in% vy$Vessel_ID)
+
+    SI <- sparsityindex(esi, nlinks = nf, nnodes = nrow(vy), "ev")
+
     p <- vy$NFisheries/sum(vy$NFisheries)
     H <- -sum(p * log(p))
     DF <- fragmentation(Ainv)
-    
+
     closeness <- igraph::closeness(gy)
     strength <- igraph::strength(gy)
-    
+
     embstats <- fleet_summary_emb_safe(gy, .01)
-    
+
     allbars <- tibble(Year = yi,
                       Component = i,
                               Vessel_ID = as.integer(names(membership(cw))),
@@ -120,19 +123,19 @@ graph_ext_fcn_ves <- function(df_choice, fisherylist_use, vessel_pairs, yi, drop
                               HullAreaTrimmed = embstats$hull_area_trimmed,
                               MedianToCentroid = embstats$median_dist_to_centroid,
                               MeanToCentroid = embstats$mean_dist_to_centroid)
-    
+
   })
-  
+
 }
-  
+
 
 # fuller ------------------------------------------------------------------
 
 graph_fuller_fcn_ves <- function(df_choice, yi, fisherylist_use, drop){
-  
-  dropfishers <- df_choice %>% 
+
+  dropfishers <- df_choice %>%
     filter(FISHERY_ID %in% drop)
-  
+
   nfisheries <- length(fisherylist_use)
   nvessels <- n_distinct(df_choice$Vessel_ID)
   bipmat <- df_choice %>%
@@ -143,35 +146,35 @@ graph_fuller_fcn_ves <- function(df_choice, yi, fisherylist_use, drop){
     pivot_wider(names_from = Vessel_ID, values_from = Weights, values_fill = 0) %>%
     column_to_rownames("FISHERY_ID") %>%
     as.matrix()
-  
+
   connectivity <- matrix(0, nvessels, nvessels)
   colnames(connectivity) <- rownames(connectivity) <- as.numeric(unique(df_choice$Vessel_ID))
   # bipmat_0 <- bipmat
   edges_fuller <- fulleredges_vessel(connectivity, bipmat, dropfishers)
   bipmat_0 <- bipmat
-  
+
   ey <- edges_fuller %>%
     filter(Weight > 0) %>%
     select(Vessel1, Vessel2, weight = Weight)
-  
+
   vy <- df_choice %>%
     filter(FISHERY_ID != "Not Fishing",
            Vessel_ID %in% c(ey$Vessel1, ey$Vessel2)) %>%
-    group_by(Vessel_ID) %>% 
+    group_by(Vessel_ID) %>%
     summarise(NWeeks = n(),
               .groups = "drop")
-  
+
   gy <- graph_from_data_frame(ey,
                               directed = F,
                               vertices = vy)
-  
-  componentuse <- tibble(comps = igraph::components(gy)$membership) %>% 
-    group_by(comps) %>% tally() %>% 
+
+  componentuse <- tibble(comps = igraph::components(gy)$membership) %>%
+    group_by(comps) %>% tally() %>%
     filter(n > 20)
-  
-  i <- unique(componentuse$comps)[2]
+
+  i <- unique(componentuse$comps)[1]
   dfgraph_out_ext <- map_dfr(unique(componentuse$comps), function(i){
-    
+
     vy <- df_choice %>%
       filter(FISHERY_ID != "Not Fishing",
              Vessel_ID %in% c(ey$Vessel1, ey$Vessel2)) %>%
@@ -179,45 +182,48 @@ graph_fuller_fcn_ves <- function(df_choice, yi, fisherylist_use, drop){
       summarise(NFisheries = n_distinct(FISHERY_ID),
                 .groups = "drop")  %>%
       select(Vessel_ID, NFisheries)
-    
+
     gy <- graph_from_data_frame(ey,
                                 directed = F,
                                 vertices = vy)
-    
-    gy <- induced_subgraph(gy, 
+
+    gy <- induced_subgraph(gy,
                            vids = which(igraph::components(gy)$membership == i))
-    vy <- vy %>% 
+    vy <- vy %>%
       filter(Vessel_ID %in% names(V(gy)))
-    
+
     A <- as_adjacency_matrix(gy, sparse=FALSE, attr="weight")
     lA <- A[lower.tri(A)]
     Ainv <- A
     Ainv[A!=0] <- 1/A[A!=0]
-    
+
     # graph level
     OnnelaClust <- ClustF(A, "undirected")
     cw <- cluster_walktrap(gy, modularity = T)
     m <- modularity(cw)
     nc <- networkcentralization(gy)
     urf <- mean(E(gy)$weight) + var(E(gy)$weight)/mean(E(gy)$weight)
-    
-    # SI <- sparsityindex(ey, nlinks = nfisheries, 
+
+    # SI <- sparsityindex(ey, nlinks = nfisheries,
     #                     nnodes = nrow(vy), F)
-    df_sub <- bind_rows(cache_dfchoice[10:14]) %>% 
+    df_sub <- bind_rows(cache_dfchoice[10:14]) %>%
       filter(Vessel_ID %in% vy$Vessel_ID)
     nf <- n_distinct(df_sub$FISHERY_ID)
-    
-    SI <- sparsityindex(ey, nlinks = nf, nnodes = nrow(vy), "fv")
-    
+    esi <- ey %>%
+      filter(Vessel1 %in% vy$Vessel_ID,
+             Vessel2 %in% vy$Vessel_ID)
+
+    SI <- sparsityindex(esi, nlinks = nf, nnodes = nrow(vy), "fv")
+
     p <- vy$NFisheries/sum(vy$NFisheries)
     H <- -sum(p * log(p))
     DF <- fragmentation(Ainv)
-    
+
     closeness <- igraph::closeness(gy)
     strength <- igraph::strength(gy)
-    
+
     embstats <- fleet_summary_emb_safe(gy, .01)
-    
+
     allbars <- tibble(Year = yi,
                       Component = i,
                       Vessel_ID = as.integer(names(membership(cw))),
@@ -240,9 +246,9 @@ graph_fuller_fcn_ves <- function(df_choice, yi, fisherylist_use, drop){
                       HullAreaTrimmed = embstats$hull_area_trimmed,
                       MedianToCentroid = embstats$median_dist_to_centroid,
                       MeanToCentroid = embstats$mean_dist_to_centroid)
-    
+
   })
-  
+
 }
 
 
